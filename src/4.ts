9b74ffc39cd8acfbf5e6c5b13d0a43e3d16658f9
Julia Cheperis <julia.cheperis@dataart.com>
2024-06-25 16:00:09 +0200

    add files

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,64 @@
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  public getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  public getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected key: Key;
+  protected tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  public comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log('Person entered the house.');
+    } else {
+      console.log('Door is closed. Cannot enter the house.');
+    }
+  }
+
+  public abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(key);
+  }
+
+  public openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log('Door is now open.');
+    } else {
+      console.log('Key does not match. Door remains closed.');
+    }
+  }
+}
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -8,4 +69,4 @@ house.openDoor(person.getKey());
 house.comeIn(person);
 
 
-export {};
+export { };
